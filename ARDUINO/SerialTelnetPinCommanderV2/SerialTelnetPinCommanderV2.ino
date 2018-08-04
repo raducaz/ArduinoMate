0ab5c15a4ca753e35374666add7acc375054cbda
Radu <raducaz@gmail.com>
2018-08-04 16:17:47 +0300

    Added the super tool of control using telnet and SerialMonitor input

---
+++ ARDUINO/SerialTelnetPinCommanderV2/SerialTelnetPinCommanderV2.ino
@@ -0,0 +1,270 @@
+#include <SPI.h>
+
+#include <Thread.h>
+#include <ThreadController.h>
+
+#include <Dns.h>
+#include <Ethernet.h>
+#include <EthernetServer.h>
+#include <EthernetUdp.h>
+
+// Satisfy IDE, which only needs to see the include statment in the ino.
+#ifdef dobogusinclude
+#include <spi4teensy3.h>
+#endif
+
+// Enter a MAC address and IP address for your controller below.
+byte mac[] = { 0x00, 0xAA, 0xBB, 0xCC, 0xDA, 0x02 };
+IPAddress ip(192,168,1,100); //<<< ENTER YOUR IP ADDRESS HERE!!!
+
+ThreadController threadsController = ThreadController();
+class MyTcpServerThread: public Thread
+{
+  static const byte SIZE = 10;
+  char receivedText[SIZE] = "";
+  char bufferText[SIZE] = "";
+  char cmdChar = 0; 
+  int pin = 0;
+  
+    char endChar = '\n';
+    EthernetServer server = EthernetServer(8080);
+    EthernetClient client = EthernetClient();
+    
+    // Function executed on thread execution
+    void run(){
+
+    server.begin(); 
+    client = server.available();
+
+    if (Serial.available()!=0) {             //Wait for user input
+      char receivedChar = Serial.read();
+      Serial.print(receivedChar);
+      
+      if(collectInput(receivedChar)){
+        Serial.println("Exec Serial CMD.");
+        execCmd();
+      }
+      
+    }
+
+    if (client) {
+      client.println("2=1 to set pin 2 to 1; ls to list all pins; use 2/1 to set pin 2 to 1 for 500ms"); //Prompt User for input
+      client.println("Serial commands cannot be sent until end of this session."); //Prompt User for input
+      while (client.connected()) {
+        if (client.available()) {
+          
+          char receivedChar = client.read();
+
+          if(collectInput(receivedChar)) {
+            // Text until \n received
+            client.println("Exec telnet CMD, see Serial monitor for details");
+            Serial.println("Exec telnet CMD.");
+
+            execCmd();
+
+            if(strncmp(receivedText, "end", 3)==0){
+              Serial.println("CLOSE CONNECTION");
+              client.println("CLOSE CONNECTION");
+              client.stop();
+              Serial.println("END LOOP");
+            }
+            
+          }
+          
+        } 
+      } 
+      
+//      Serial.println();
+//      Serial.println("CLOSE CONNECTION"); 
+      //client.stop();
+//      Serial.println("END LOOP");
+
+    }
+    else
+    {
+      // No client, server not available()
+//      Serial.println("No client, server stopped.");
+    }  
+
+    runned();
+  }
+  bool collectInput(const char receivedChar)
+  {
+    if (receivedChar=='\n')
+    {
+      receivedText[0]=0;
+      strcpy(receivedText, bufferText);
+      bufferText[0]=0;
+
+      Serial.print("Got:");Serial.print(receivedText);Serial.print(" CMD:");Serial.print(cmdChar);Serial.print(" Pin:");Serial.println(pin);
+      client.print("Got:");client.print(receivedText);client.print(" CMD:");client.print(cmdChar);client.print(" Pin:");client.println(pin);
+      
+      return true;
+    }
+    else
+    {
+      if(receivedChar=='=' || receivedChar=='?' || receivedChar=='/')
+      {
+        cmdChar = receivedChar;
+
+        if(bufferText[0]==0)
+          pin = -1;
+        else
+          pin = atoi(bufferText);
+          
+        bufferText[0]= 0;// Empty string
+      }
+      else
+      {
+        size_t len = strlen(bufferText);
+        if (len < SIZE-1)
+        {
+          bufferText[len] = receivedChar;
+          bufferText[len + 1] = 0;
+        }
+        else
+        {
+          Serial.println("Max meesage len exceeded !! Clearing receivedText.");
+          bufferText[0]=0;
+        }
+      }
+    }
+
+     return false;
+  }
+  void setPin(byte pin, int value)
+  {
+      Serial.print("set pin:");Serial.print(pin);
+      Serial.print(" to value:");Serial.println(value);
+      
+      client.print("set pin:");client.print(pin);
+      client.print(" to value:");client.println(value);
+    
+      digitalWrite(pin,value);
+  }
+  void setPinTmp(byte pin, int value, int ms)
+  {
+      byte v = digitalRead(pin);
+      
+      Serial.print("set pin:");Serial.print(pin);
+      Serial.print(" to value (for ");Serial.print(ms);Serial.print("ms):");Serial.println(value);
+      
+      client.print("set pin:");client.print(pin);
+      client.print(" to value (for ");client.print(ms);client.print("ms):");client.println(value);
+      
+      digitalWrite(pin,value);
+      wait(ms);
+      digitalWrite(pin,v);
+  
+      Serial.print("reverted pin:");Serial.print(pin);
+      Serial.print(" to value:");Serial.println(v);
+      
+      client.print("reverted pin:");client.print(pin);
+      client.print(" to value:");client.println(v);
+  }
+  void getPin(byte pin)
+  {
+      Serial.print("pin:");Serial.print(pin);
+      Serial.print(" is:");Serial.println(digitalRead(pin));
+      
+      client.print("pin:");client.print(pin);
+      client.print(" is:");client.println(digitalRead(pin));
+  }
+  void execCmd()
+  {
+    int value = 0;
+    
+    if(strncmp(receivedText, "ls", 2)==0)
+    {
+      for(byte i=0;i<=13;i++)
+      {
+        Serial.print(i);Serial.print("=");Serial.println(digitalRead(i));
+        client.print(i);client.print("=");client.println(digitalRead(i));
+        
+      }
+    }
+    else if(cmdChar == '=')
+    {
+      value = atoi(receivedText);
+
+      for(byte i=(pin==-1?0:pin);i<=(pin==-1?13:pin);i++)
+      {
+        setPin(i, value);
+      }
+    }
+    else if(cmdChar == '/')
+    {
+      value = atoi(receivedText);
+    
+      for(byte i=(pin==-1?0:pin);i<=(pin==-1?13:pin);i++)
+      {
+        setPinTmp(i, value, 500);
+      }
+    }
+    else if(cmdChar == '?')
+    {
+      for(byte i=(pin==-1?0:pin);i<=(pin==-1?13:pin);i++)
+      {
+        getPin(i);
+      }
+    }
+    else
+    {
+      Serial.println("no command");
+    }
+  }
+  void wait(unsigned int msInterval)
+  {
+      unsigned long waitStart = millis();
+      //Serial.print("wait:");Serial.print(msInterval);Serial.println(waitStart);
+  
+      unsigned long current = millis();
+      while((current - waitStart)< msInterval) 
+      {
+        current = millis();
+        //Serial.print("Current:");Serial.println(current);
+      }; // wait until 
+      
+     // Serial.print("done wait");Serial.println(millis());
+  } 
+};
+
+void setup() {
+  Serial.begin(9600);
+
+  // OUTPUT PINS
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+
+  // start the Ethernet connection and the server:
+  Ethernet.begin(mac, ip);
+  delay(1000);
+  setupTcpServerThread();
+
+  Serial.println("2=1 to set pin 2 to 1; ls to list all pins; use 2/1 to set pin 2 to 1 for 500ms"); //Prompt User for input
+  Serial.println("telnet 192.168.1.100 8080 - monitor output in Serial Monitor."); //Prompt User for input
+}
+void setupTcpServerThread()
+{
+  MyTcpServerThread tcpServerThread = MyTcpServerThread();
+  // Set the interval the thread should run in loop
+  tcpServerThread.setInterval(1); // in ms
+  threadsController.add(&tcpServerThread);
+}
+
+void loop() {
+
+  threadsController.run();
+}
+

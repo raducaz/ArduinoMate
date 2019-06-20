89565862b21fc672d3fe712709b46f2f638e18f8
Radu <raducaz@gmail.com>
2019-06-20 10:57:44 +0300

    WatchDog v1.0 ready to live

--- ARDUINOV2/WatchDog/src/main.cpp
+++ ARDUINOV2/WatchDog/src/main.cpp
@@ -1,10 +1,11 @@
 #include <Arduino.h>
 
 byte resetSelf = 13;
-byte clientIN1 = 2;
-byte clientOUT1 = 3;
-int clientState1 = 0;
-unsigned long lastChangeTime1;
+
+// This is the matrix of clients with their states
+// clients[0] = [0,1234234] - client on pin 2 is 0 from 1234234 timestamp
+// clients[1] = [0,1234234] - client on pin 4 is 0 from 1234234 timestamp
+unsigned long clients[6][2];
 
 void setup() {
   Serial.begin(9600);
@@ -13,40 +14,50 @@ void setup() {
   pinMode(resetSelf, OUTPUT);
   digitalWrite(resetSelf, 1);
   
-  pinMode(clientIN1, INPUT_PULLUP);
-  pinMode(clientOUT1, OUTPUT);
-  digitalWrite(clientOUT1, 1);
+  for(byte b=0;b<6;b++)
+  {
+    pinMode(2*(b+1), INPUT_PULLUP);
+    pinMode(2*(b+1)+1, OUTPUT);
+    digitalWrite(2*(b+1)+1, 1);
+  }
 }
 
 void loop() {
   delay(1000);
 
-  int currentState = digitalRead(clientIN1);
-  if(currentState==clientState1)
+  for(byte b=0;b<6;b++)
   {
-    if(millis() - lastChangeTime1 > 10*pow(10, 3))
+    Serial.println(b);
+
+    byte clientIN = 2*(b+1);
+    byte clientOUT = clientIN+1;
+    int currentState = digitalRead(clientIN);
+    if(currentState==clients[b][0])
     {
-      Serial.println("reset");
-      // reset client
-      digitalWrite(clientOUT1, 0);
-      delay(500);
-      digitalWrite(clientOUT1, 1);
-      lastChangeTime1 = millis();
+      if(millis() - clients[b][1] > 10*pow(10, 3))
+      {
+        Serial.println("reset");
+        // reset client
+        digitalWrite(clientOUT, 0);
+        delay(500);
+        digitalWrite(clientOUT, 1);
+        clients[b][1] = millis();
+      }
+
+      Serial.print("same");Serial.print(currentState);Serial.println(clients[b][0]);
+
+    } 
+    if(currentState!=clients[b][0])
+    {
+      Serial.print("change");Serial.print(currentState);Serial.println(clients[b][0]);
+
+      clients[b][0] = digitalRead(clientIN);
+      clients[b][1] = millis();
     }
-
-    Serial.print("same");Serial.print(currentState);Serial.println(clientState1);
-
-  } 
-  if(currentState!=clientState1)
-  {
-    Serial.print("change");Serial.print(currentState);Serial.println(clientState1);
-
-    clientState1 = digitalRead(clientIN1);
-    lastChangeTime1 = millis();
   }
   
   // Reset self if millis > 1 day - is not working this way
-  if(millis() > 24*3600*pow(10, 3))
+  if(millis() > (unsigned long)24*3600*pow(10, 3))
   {
     digitalWrite(resetSelf,0);
   }
